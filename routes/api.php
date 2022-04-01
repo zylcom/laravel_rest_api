5fd2ad671d1e12dda434f6396584c9b144f20ad5
sabilillah <sabilillah272@gmail.com>
2022-04-01 14:26:20 +0700

    create api routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('register', [RegisterController::class, 'register']);
+Route::post('login', [RegisterController::class, 'login']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::resource('posts', PostController::class);
 });
