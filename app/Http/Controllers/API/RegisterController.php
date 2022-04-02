233a3dca97683179e5feb5d499a1484465438c7a
sabilillah <sabilillah272@gmail.com>
2022-04-02 13:50:43 +0700

    update RegisterController.php

--- app/Http/Controllers/API/RegisterController.php
+++ app/Http/Controllers/API/RegisterController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\API\BaseController as BaseController;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Validator;
+use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends BaseController
 {
@@ -35,8 +35,12 @@ class RegisterController extends BaseController
   public function login(Request $request)
   {
     if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+
+      /**
+       * @var User $user
+       */
       $user = Auth::user();
-      $success['token'] = $user->createToken('MyApp')->accessToken;
+      $success['token'] = $user->createToken('My Token')->accessToken;
       $success['name'] = $user->name;
 
       return $this->sendResponse($success, 'User login successfully.');
