15044c924500e3c29e6a29f34897af91b80711d9
sabilillah <sabilillah272@gmail.com>
2022-04-06 15:44:25 +0700

    update PostResource.php

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -18,6 +18,7 @@ class PostResource extends JsonResource
             'id' => $this->id,
             'title' => $this->title,
             'content' => $this->content,
+            'image' => $this->image,
             'created_at' => $this->created_at->format('d/m/Y'),
             'updated_at' => $this->updated_at->format('d/m/Y'),
         ];
